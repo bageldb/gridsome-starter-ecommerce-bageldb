907f712b0a6f1509f900326b9e0fd2c1b7f9e120
Natanel Young <natanel@bagelstudio.co.il>
2020-12-07 16:53:05 +0200

    dropdown

---
+++ src/components/dropdown.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>
+      {{ title }}
+      <span class="arrow-down">›</span>
+    </p>
+    <div cla>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dropdown",
+  props: { title: String },
+};
+</script>
+
+<style scoped>
+.arrow-down {
+  display: inline-block;
+  padding: 5px;
+  transform: rotate(90deg);
+}
+</style>
